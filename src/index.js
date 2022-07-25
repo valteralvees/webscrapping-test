d7e73ee5d111f2a5b6c21d25d77c5fa70b62a09c
valter-alves <108811308+valter-alves@users.noreply.github.com>
2022-07-25 11:08:09 -0300

    Init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const cheerio = require('cheerio');
+const request = require('request-promise');
+const fs = require('fs');
+const {Parser} = require('json2csv');
+
+async function extracao(){
+    const html = await request.
+}
